38e04d9701b9da96e69648ec27ba57d353966c8c
An-Li Alt Ting <anlialtting@gmail.com>
2015-08-27 08:07:47 +0800

    .

--- htmleditor.js
+++ htmleditor.js
@@ -1,28 +1,28 @@
 'use strict'
 !function(){
+var list_tags=[
+    'A',
+    'DIV',
+    'H1',
+    'H2',
+    'H3',
+    'H4',
+    'H5',
+    'H6',
+    'P',
+    'SPAN',
+    'TABLE',
+    'THEAD',
+    'TBODY',
+    'TR',
+    'TH',
+    'TD',
+]
 window.HTMLEditor=HTMLEditor
 function HTMLEditor(parentnode){
-    var list_tags=[
-        'A',
-        'DIV',
-        'H1',
-        'H2',
-        'H3',
-        'H4',
-        'H5',
-        'H6',
-        'P',
-        'SPAN',
-        'TABLE',
-        'THEAD',
-        'TBODY',
-        'TR',
-        'TH',
-        'TD',
-    ]
     parentnode.contentEditable=true
-    this.html=function(){
-        return parentnode.innerHTML
-    }
+}
+HTMLEditor.prototype.html=function(){
+    return parentnode.innerHTML
 }
 }()
